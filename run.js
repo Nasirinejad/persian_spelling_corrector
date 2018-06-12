eedef15b0f4738f791fc343fc0f7c8f736926c17
Raya <marsav98@gmail.com>
2018-06-12 18:48:59 +0430

    Add files via upload

---
+++ run.js
@@ -0,0 +1,20 @@
+﻿const http = require('http');
+const path = require('path');
+const pr = require('./lib/core.js');
+const ui = require('./ui/core.js');
+var server = http.createServer(function(req, res) {
+    pr.set(req, res);
+    ui.set(req, res);
+    Commentator(req);
+});
+
+server.listen(3000, '127.0.0.1');
+
+function Commentator(req) {
+    ext = path.extname(req.url);
+    if (ext == '.prc') {
+        pr.prcRequests();
+    }else {
+        ui.interpreter();
+    }
+}
